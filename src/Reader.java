7708b24d2d33ef15c87f327febfd2258c5d7c829
QuentinBay <quentin.bayart45430@gmail.com>
2016-02-08 19:16:40 +0100

    Adds method to check if a point has been registered

--- src/Reader.java
+++ src/Reader.java
@@ -56,6 +56,9 @@ public class Reader {
 				for(int i=0; i<sCurrentLine.length(); i++)
 				{
 					c = sCurrentLine.charAt(i);
+					if (i==0){
+						System.out.println(c+" ");
+					}
 					
 					if (c == '#')
 					{
@@ -79,25 +82,23 @@ public class Reader {
 							count = 0;
 						}
 					}
-					
-					// END OF LINE, CHECK IF NEED TO BE REGISTERED
-					if (i==sCurrentLine.length()-1)
-					{
-						if (continuousLine)
-						{
-							// END OF CONTINUOUS LINE, MUST REGISTER IT
-							xFirstPos.add(i-count); //C1
-							yFirstPos.add(rowNumber-1); //R1
-							
-							xSecondPos.add(i-1); //C2
-							ySecondPos.add(rowNumber-1); //R2
-							
-							// RESTART COUNT
-							continuousLine = false;
-							count = 0;
-						}
-					}
 				}
+				
+				// END OF LINE, CHECK IF NEED TO BE REGISTERED
+				if (continuousLine)
+				{
+					// END OF CONTINUOUS LINE, MUST REGISTER IT
+					xFirstPos.add(sCurrentLine.length()-count); //C1
+					yFirstPos.add(rowNumber-1); //R1
+					
+					xSecondPos.add(sCurrentLine.length()-1); //C2
+					ySecondPos.add(rowNumber-1); //R2
+					
+				}
+				// RESTART COUNT
+				continuousLine = false;
+				count = 0;
+				
 				// COUNT FOR R1=R2 (=rowNumber)
 				rowNumber++;
 			}
@@ -138,7 +139,7 @@ public class Reader {
 			// WRITE COMMANDS IN THE FILE
 			for(int i=0; i<xFirstPos.size(); i++)
 			{
-				content = "PAINT_LINE "+xFirstPos.get(i)+" "+yFirstPos.get(i)+" "+xSecondPos.get(i)+" "+ySecondPos.get(i)+"\n";
+				content = "PAINT_LINE "+yFirstPos.get(i)+" "+xFirstPos.get(i)+" "+ySecondPos.get(i)+" "+xSecondPos.get(i)+"\n";
 				bw.write(content);
 			}
 			
@@ -149,7 +150,78 @@ public class Reader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-			
-		
 	}
+	
+/**
+ * Draws the painting from the arrays to check the painting
+ * @param fileName
+ */
+	public void checkPainting(String fileName)
+	{
+		try {
+
+			String content = null;
+
+			File file = new File(fileName);
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			// INIT : NUMBER OF LINES
+			content = xFirstPos.size()+"\n";
+			bw.write(content);
+			
+			int xi = 0;
+			
+			// WRITE COMMANDS IN THE FILE
+			for(int yi=0; yi<14; yi++)
+			{
+				for(xi=0; xi<80; xi++)
+				{
+					if ()
+					{
+						
+					}
+					else
+					{
+						
+					}
+					content+=".";
+				}
+				content = "PAINT_LINE "+yFirstPos.get(i)+" "++" "+ySecondPos.get(i)+" "+xSecondPos.get(i)+"\n";
+				bw.write(content);
+			}
+			
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+/**
+ * Returns true if [xi, yi] is included in one of the lines registered while reading
+ * @param xi
+ * @param yi
+ * @return
+ */
+	public boolean isPartOf (int xi, int yi)
+	{
+		for(int i=0; i<xFirstPos.size(); i++)
+		{
+			if(xi >= xFirstPos.get(i) && xi <= xSecondPos.get(i) && yi == yFirstPos.get(i))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
