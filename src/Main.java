149dca753d091dd2620a34a94a67e2c7ea6cfe58
QuentinBay <quentin.bayart45430@gmail.com>
2016-02-08 19:28:57 +0100

    Works for logo & right_angle

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         public static void main(String[] args) {
 
             Reader r = new Reader();
-            r.paint("logo.in","commands.out", "check.in");
+            r.paint("right_angle.in","right_angle.out", "check.in");
         }
 
 }
